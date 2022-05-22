698e2e277703bea530f66f0bb21edcf70385cafb
gustavo <gugupf10@gmail.com>
2022-05-22 14:08:52 -0300

    first commit

---
+++ src/main/java/com/example/demo/controller/CarController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Car;
+import com.example.demo.model.kafka.TopicProducer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping(path = "/car")
+public class CarController {
+
+    @Autowired
+    private TopicProducer topicProducer;
+
+    @PostMapping
+    public void addQueueCars(@RequestBody Car car) {
+        topicProducer.send(car);
+        log.info("Car stored in queue kafka");
+    }
+}
---
+++ src/main/java/com/example/demo/model/kafka/TopicProducer.java
@@ -0,0 +1,23 @@
+package com.example.demo.model.kafka;
+
+import com.example.demo.model.Car;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class TopicProducer {
+    @Value("${topic.name.producer}")
+    private String topicName;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void send(Car message) {
+        log.info("Sended payload: {}", message);
+        kafkaTemplate.send(topicName, message.toString());
+    }
+}
