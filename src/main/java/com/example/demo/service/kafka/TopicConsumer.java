c7ba749450866c8134cd47a2c9ce8df2ecb4f0fd
gustavo <gugupf10@gmail.com>
2022-05-25 16:56:30 -0300

    Serialization and Deserialization data between producer and consumer

--- src/main/java/com/example/demo/service/kafka/TopicConsumer.java
+++ src/main/java/com/example/demo/service/kafka/TopicConsumer.java
@@ -1,5 +1,7 @@
 package com.example.demo.service.kafka;
 
+import com.example.demo.model.Car;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -16,12 +18,20 @@ public class TopicConsumer {
     private String topicName;
 
     @KafkaListener(topics = "${topic.name.consumer}", groupId = "group_id")
-    public ConsumerRecord<String, String> consume(ConsumerRecord<String, String> payload) {
+    public ConsumerRecord<String, String> consume(ConsumerRecord<String, String> payload) throws Exception {
         log.info("Topic: {}", topicName);
         log.info("key: {}", payload.key());
         log.info("Headers: {}", payload.headers());
         log.info("Partion: {}", payload.partition());
         log.info("Order {}", payload.value());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Car car = objectMapper.readValue(payload.value(), Car.class);
+
+        System.out.println("----------------------------");
+        System.out.println(car.getBrand());
+        System.out.println(car.getModel());
+        System.out.println(car.getYear());
 
         return payload;
     }
--- src/main/java/com/example/demo/service/kafka/TopicProducer.java
+++ src/main/java/com/example/demo/service/kafka/TopicProducer.java
@@ -1,6 +1,8 @@
 package com.example.demo.service.kafka;
 
 import com.example.demo.model.Car;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,8 +18,17 @@ public class TopicProducer {
     private String topicName;
     private final KafkaTemplate<String, String> kafkaTemplate;
 
-    public void send(Car message) {
-        log.info("Sended payload: {}", message);
-        kafkaTemplate.send(topicName, message.toString());
+    public void send(Car car) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String carFormattedString = null;
+
+        try {
+            carFormattedString = objectMapper.writeValueAsString(car);
+        } catch (JsonProcessingException ex) {
+            ex.printStackTrace();
+        }
+
+        kafkaTemplate.send(topicName, carFormattedString);
+        log.info("Sended payload: {}", carFormattedString);
     }
 }
